4d7a062fdbe83d8f0bd6f4e848149ed5c0877a25
Cengiz Cimen <z5359299@ad.unsw.edu.au>
2024-10-02 16:08:59 +1000

    chore: updated README.md to include installation instructions

--- src/mesh_generate.py
+++ src/mesh_generate.py
@@ -13,7 +13,7 @@ def generate_mesh_from_images(path: str, out:str = None):
     - path (str): path to the folder of images
     - out (str) (optional): output directory for mesh
 
-    ## Output
+    ## Return
     A .obj file that can be used within nvidia omniverse.
 
     ##  Example
@@ -31,6 +31,25 @@ def generate_mesh_from_images(path: str, out:str = None):
     pass
 
 def remove_background_from_images(image):
+    '''
+    ## Args
+    - image: image file
+
+    ## Return
+    A .obj file that can be used within nvidia omniverse.
+
+    ##  Example
+    ```
+    import remove_background_from_images from mesh_generate
+
+    image = get_image()
+
+    image_new = remove_background_from_images(image)
+
+    ```
+    '''
+
+    (image)
 
     pass
 
